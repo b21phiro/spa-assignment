aa460cd24e07d708e300cd438a50b0e4d144e67c
PhilipRosenqvist <philip.rosenqvist@outlook.com>
2022-11-11 09:35:34 +0100

    Fixed form

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,17 @@
 const html = `
-<h1>Logga in!</h1>
+<section>
+    <h1>Logga in</h1>
+    <p>Som inloggad användare kan du boka tider hos oss snabbt och enkelt!</p>    
+</section>
+<form method="post">
+
+    <!--Mejl address-->
+    <label>
+        <p>Mejl address</p>
+        
+    </label>
+    
+</form>
 `;
 
 export default (main) =>
