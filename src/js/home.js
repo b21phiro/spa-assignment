957e85eb3c2e2dc6c714cd617ce9914612ef09c7
PhilipRosenqvist <philip.rosenqvist@outlook.com>
2022-11-20 09:15:32 +0100

    Fixade till framsidan

--- src/js/home.js
+++ src/js/home.js
@@ -1,5 +1,9 @@
 const html = `
-<h1>Framsida!</h1>
+<section class="page">
+    <h1 class="page_title">Behöver din frilla renoveras?</h1>
+    <p class="page_text">Vi har de bästa frisörerna i stan, boka en tid hos oss redan idag!</p>
+    <a class="button_solid" href="/boka-tid" title="Boka en tid">Boka en tid</a>
+</section>
 `;
 
 export default (main) =>
